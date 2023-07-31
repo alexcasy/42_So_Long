c2fceee9bdf83050e633213e819db95190569808
Alexcasy <129069125+alexcasy@users.noreply.github.com>
2023-07-31 15:07:36 +0200

    Add files via upload

--- read_map.c
+++ read_map.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   read_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acasale <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/31 12:53:16 by acasale           #+#    #+#             */
-/*   Updated: 2023/07/31 12:54:38 by acasale          ###   ########.fr       */
+/*   Created: 2023/07/31 14:41:42 by acasale           #+#    #+#             */
+/*   Updated: 2023/07/31 14:42:33 by acasale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
 
-int	main(int argc, char **argv)
-{
-	t_game	game;
 
-	if (argc != 2)
-	{
-		ft_printf("Error! Please load a map to begin!, \n");
-		exit(1);
-	}
-	read_map ();
-}
